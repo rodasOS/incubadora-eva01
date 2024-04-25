eaaf13ca8744a1e3336380fc9543cf50652b4457
rodasOS <139723305+rodasOS@users.noreply.github.com>
2024-04-25 08:30:49 -0600

    Changing the fetch for the import

--- src/pages/api/ejemplo.json.ts
+++ src/pages/api/ejemplo.json.ts
@@ -1,10 +1,21 @@
-const response = await fetch('http://incubadora-eva01.vercel.app/src/pages/api/pepe.json');
-const data = await response.json();
-console.log(data)
+import data from './pepe.json'
+// const response = await fetch('http://localhost:4321/src/pages/api/pepe.json');
+// const data = await response.json();
+interface User {
+	author: string;
+	body: string;
+}
+
+// const res = await fetch('./pepe.json');
+// if (res.headers.get('content-type')?.includes('application/json')) {
+// 	let data = await res.json();
+// 	// Utiliza los datos aquí
+// } else {
+// 	console.error('No se recibió una respuesta JSON');
+// }
 
 export async function GET() {
 	return new Response(
 		JSON.stringify(data),
 	);
 }
-
