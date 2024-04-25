ee163e50576d3076a91978162f691c5ffe21c6b9
rodasOS <139723305+rodasOS@users.noreply.github.com>
2024-04-25 10:21:26 -0600

    Cambiando el JSON a public/

--- src/pages/api/ejemplo.json.ts
+++ src/pages/api/ejemplo.json.ts
@@ -1,21 +1,15 @@
-import data from './pepe.json'
+import data from '../../../public/pepe.json'
 // const response = await fetch('http://localhost:4321/src/pages/api/pepe.json');
 // const data = await response.json();
+console.log('------------------->>>>> \n')
+// console.log(data);
 interface User {
 	author: string;
 	body: string;
 }
 
-// const res = await fetch('./pepe.json');
-// if (res.headers.get('content-type')?.includes('application/json')) {
-// 	let data = await res.json();
-// 	// Utiliza los datos aquí
-// } else {
-// 	console.error('No se recibió una respuesta JSON');
-// }
-
 export async function GET() {
 	return new Response(
-		JSON.stringify(data),
+		 JSON.stringify(data),
 	);
 }
